f0f50222d0db57323b6e10322018d993e738a875
noah-azz <157928577+noah-azz@users.noreply.github.com>
2026-05-03 11:52:23 -0400

    Renamed module in tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_noah_azz_demo user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
